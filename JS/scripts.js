b1a1ec15a40c671062b5d6c48a893c71b24b9302
Mikah Mathews <mikah@outofband.org>
2020-03-30 10:32:48 -0700

    It will add complete JS to collect and show user input, complete HTML to display User Input in UL.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+  $("verifyForm").hide();
   $("form#survey").submit(function(event) {
+    // Back end collecting input
     var name = $("#name").val();
     var color = $("#color").val();
     var animal = $("#animal").val();
@@ -7,12 +9,18 @@ $(document).ready(function() {
     var song = $("#song").val();
 
     var array = [name, color, animal, food, song];
+    var newArray = [];
 
-    console.log(array[0]);
-    console.log(array[1]);
-    console.log(array[2]);
-    console.log(array[3]);
-    console.log(array[4]);
+    newArray.push(array[1]); // this is color
+    newArray.push(array[0]); // this is name
+    newArray.push(array[2]); // this is animal (ﾉ °益°)ﾉ 彡 ┻━┻
+
+    // Front end displaying input
+    $(".color").text(newArray[0]);
+    $(".name").text(newArray[1]);
+    $(".animal").text(newArray[2]);
+
+    $("#verifyForm").show();
 
     event.preventDefault();
   })
